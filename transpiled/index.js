0a36bd862962a1ebd57d9e5ac5c67442e2e2a442
VictorMendes06 <92352429+VictorMendes06@users.noreply.github.com>
2022-07-07 23:35:57 -0300

    Add files via upload

---
+++ transpiled/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_react["default"].render( /*#__PURE__*/_react["default"].createElement("h1", {
+  id: "title"
+}, /*#__PURE__*/_react["default"].createElement("span", null, "Hello Word !!!!!!")), document.getElementById("root"));
